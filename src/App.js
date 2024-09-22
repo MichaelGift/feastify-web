001d2f83df5b7875e6731bfa12910e0484b0f1c5
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-09-22 10:39:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
+import Landingpage from './components/landing page/landingpage';
+import EventType from './components/events/events';
+import ServiceDay from './components/service/service';
+import ServiceSchedule from './components/service/serviceschedule';
+import ServiceQuality from './components/service/serviceQuality';
+import DietaryRestrictions from './components/service/diet';
+import Footer from './components/footer/footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/event-type" element={<EventType />} />
+          <Route path="/service-day" element={<ServiceDay />} />
+          <Route path="/service-schedule" element={<ServiceSchedule />} />
+          <Route path="/service-quality" element={<ServiceQuality />} />  
+          <Route path="/diet-restrictions" element={<DietaryRestrictions/>} />        
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
